ace7086a13d6dcbb744e7e0296838d5cb6f3dea9
hieuhvh1994 <hieudaohn94@gmail.com>
2022-01-06 09:12:24 +0700

    Added front-end

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {SignUpForm} from '../model/SignUpForm';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  // API_LOCAL
+  // private API_SIGNUP = environment.API_LOCAL + 'signup';
+  // API_SERVER
+  private API_SIGNUP = environment.API_SERVER + 'signup';
+  constructor(private http: HttpClient) { }
+
+  signup(signUpForm: SignUpForm): Observable<any> {
+    return this.http.post(this.API_SIGNUP, signUpForm);
+  }
+}
